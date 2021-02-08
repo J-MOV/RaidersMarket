9145ebd693a9e4e1d000d50188585b88633ebc98
Viktor753 <72790285+Viktor753@users.noreply.github.com>
2021-02-08 11:32:08 +0100

    Player Health, Stats, Combat Scripts
    Created PlayerHealth, PlayerCombat, PlayerStatsSO scripts

---
+++ Assets/PlayerScripts/Monobehaviours/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth, maxHealth;
+    int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        //Audioclip?, particle effect?
+
+        if(currentHealth <= 0)
+        {
+            //Die
+            PlayerDeath();
+        }
+    }
+
+
+    void PlayerDeath()
+    {
+        Debug.Log("Player died");
+    }
+}
---
+++ Assets/PlayerScripts/Monobehaviours/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerHealth), typeof(PlayerCombat))]
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private PlayerStatsSO playerStats;
+
+    PlayerHealth playerHealth;
+    PlayerCombat playerCombat;
+
+    private void Awake()
+    {
+        playerHealth = GetComponent<PlayerHealth>();
+        playerCombat = GetComponent<PlayerCombat>();
+
+        AssignStatsToPlayer();
+    }
+
+
+    void AssignStatsToPlayer()
+    {
+        if(playerStats == null)
+        {
+            Debug.LogWarning("No player stats were found!");
+            return;
+        }
+
+        //Player health
+        playerHealth.startingHealth = playerStats.startingHealth;
+        playerHealth.maxHealth = playerStats.maxHealth;
+
+        //Player combat
+        playerCombat.baseDamage = playerStats.baseDamage;
+
+        Debug.Log("Assigned stats to player");
+    }
+}
