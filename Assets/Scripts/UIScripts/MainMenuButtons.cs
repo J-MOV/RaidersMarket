b98cb223700a4907a8d19449ec9d43c8077e64e1
Melon332 <thederpking@outlook.com>
2021-02-09 10:49:38 +0100

    Made it so that you can start the game, the marketplace gets loaded and then when it's finished loading it will activate that scene and send the player to the marketplace.

---
+++ Assets/Scripts/UIScripts/MainMenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] GameObject mainPanel;
+    float waitTillAnimationFinished = 3f;
+
+    public void StartGame()
+    {
+        StartCoroutine(RemovePanel());
+        //TODO: Let the game begin.
+    }
+
+
+
+    public void MarketPlaceOpened()
+    {
+        //TODO: Play animation for closing the menu
+        StartCoroutine(RemovePanel());
+        SceneManager.LoadSceneAsync("Marketplace");
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Marketplace"));
+    }
+
+    public void OptionsMenuOpened()
+    {
+
+    }
+    IEnumerator RemovePanel()
+    {
+        //TODO: Play animation for closing the menu
+        yield return new WaitForSeconds(waitTillAnimationFinished);
+        mainPanel.SetActive(false);
+    }
+}
