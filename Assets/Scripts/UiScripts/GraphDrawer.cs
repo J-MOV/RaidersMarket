07512bf75f04f691b098af575c812cac80594485
Tignaltus <78477407+Tignaltus@users.noreply.github.com>
2021-02-12 14:33:20 +0100

    Graph now wokring
    The Graph is working but could use some tweeking as it doesn't work exactly as intended.

--- Assets/Scripts/UiScripts/GraphDrawer.cs
+++ Assets/Scripts/UiScripts/GraphDrawer.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class GraphDrawer : MonoBehaviour
 {
     public List<float> dataValues;
+    private List<Transform> transformList;
 
     [SerializeField] private Sprite anchorSprite;
+    [SerializeField] private LineRender LineDrawer;
+
     private RectTransform graphContainer;
+
 
     private void Awake()
     {
         graphContainer = transform.Find("Container").GetComponent<RectTransform>();
 
-        dataValues = new List<float>() { 10, 50};
+        dataValues = new List<float>() { 10, 50, 350, 160, 300, 220};
+        transformList = new List<Transform>();
 
         AnchorSorter(dataValues);
     }
@@ -33,7 +38,7 @@ public class GraphDrawer : MonoBehaviour
 
     private void ConnectAnchors(List<float> anchorTargets)
     {
-        for(int i = 1; i < anchorTargets.Count; i++)
+        /*for(int i = 1; i < anchorTargets.Count; i++)
         {
             Vector2 currentValue = new Vector2(100 * i, anchorTargets[i]);
             Vector2 previousValue = new Vector2(100 * (i - 1), anchorTargets[i - 1]);
@@ -42,19 +47,21 @@ public class GraphDrawer : MonoBehaviour
             anchorConnection.transform.SetParent(graphContainer, false);
 
             Vector2 angle = (currentValue - previousValue).normalized;
-            float size = Vector2.Distance(currentValue, previousValue);
+            float distance = Vector2.Distance(currentValue, previousValue);
 
             RectTransform rect = anchorConnection.GetComponent<RectTransform>();
+        }*/
 
-            rect.anchoredPosition = angle * size;
-        }
+        LineDrawer.ConnectLine(transformList);
     }
 
     private void CreateAnchor(Vector2 anchorLocation)
     {
-        GameObject anchorPoint = new GameObject("AnchorPoint", typeof(GameObject));
+        GameObject anchorPoint = new GameObject("AnchorPoint");
 
         anchorPoint.transform.SetParent(graphContainer, false);
-        anchorPoint.transform.localPosition = new Vector3(anchorLocation.x, anchorLocation.y, 0);
+        anchorPoint.transform.localPosition = new Vector3(anchorLocation.x, anchorLocation.y, -10);
+
+        transformList.Add(anchorPoint.transform);
     }
 }
