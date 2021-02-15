807d53e8d8e7422446ac71083a40cf410e59bada
Tignaltus <78477407+Tignaltus@users.noreply.github.com>
2021-02-15 15:22:50 +0100

    Account Level-display
    Started working out a good way to implement Account level and displaying it.

---
+++ Assets/Scripts/UiScripts/AccountLevelHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AccountLevelHandler
+{
+    private static int accountLevel;
+
+    private static int eXP;
+
+    public static List<int> mileStones;
+
+    public static void AddExperience(int ammount)
+    {
+        eXP += ammount;
+
+        foreach(var level in mileStones)
+        {
+            if(eXP >= level)
+            {
+                eXP -= level;
+                LevelUp();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the current Account Experience
+    /// </summary>
+    public static double Experience()
+    {
+        return eXP;
+    }
+
+    /// <summary>
+    /// Returns the current Account Level
+    /// </summary>
+    public static int Level()
+    {
+        return accountLevel;
+    }
+
+    static void LevelUp()
+    {
+        accountLevel++;
+    }
+
+}
---
+++ Assets/Scripts/UiScripts/LevelBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelBar : MonoBehaviour
+{
+    private Slider targetSlider;
+    private Text targetText;
+
+    private void Start()
+    {
+        targetSlider = transform.GetComponent<Slider>();
+        targetText = transform.GetComponent<Text>();
+    }
+
+    private void setLevel()
+    {
+        for(int i = 0; i < AccountLevelHandler.mileStones.Count; i++)
+        {
+            if(i == AccountLevelHandler.Level())
+            {
+                targetSlider.maxValue = AccountLevelHandler.mileStones[AccountLevelHandler.Level()];
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
