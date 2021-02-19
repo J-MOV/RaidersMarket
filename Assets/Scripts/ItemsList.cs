d1db8365eb3a2a0c2702fa95413acf34efc38a4f
Olle Kaiser <yogsther@gmail.com>
2021-02-19 10:35:51 +0100

    Inventory and Server start

---
+++ Assets/Scripts/ItemsList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsList : MonoBehaviour
+{
+    
+}
+
+public class Item {
+    //public Item(string name, )
+}
